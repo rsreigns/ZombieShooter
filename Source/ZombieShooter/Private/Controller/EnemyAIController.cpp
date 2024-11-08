b569bebc81e437b090f544e52d3f11dc6b805bec
Rahul sharma <sharma.abcdrahul@gmail.com>
2024-11-09 00:48:05 +0530

    WIP AI

--- Source/ZombieShooter/Private/Controller/EnemyAIController.cpp
+++ Source/ZombieShooter/Private/Controller/EnemyAIController.cpp
@@ -2,17 +2,51 @@
 
 
 #include "Controller/EnemyAIController.h"
+#include "Navigation/CrowdFollowingComponent.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "BehaviorTree/BehaviorTree.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISenseConfig_Hearing.h"
+#include "Perception/AISenseConfig_Damage.h"
 
-AEnemyAIController::AEnemyAIController()
+
+AEnemyAIController::AEnemyAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
 {
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	HearConfig = CreateDefaultSubobject<UAISenseConfig_Hearing>(TEXT("HearingConfig"));
+	DamageConfig = CreateDefaultSubobject<UAISenseConfig_Damage>(TEXT("DamageConfig"));
+
 	PerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
-	PerceptionComp->OnTargetPerceptionInfoUpdated.AddDynamic(this, &ThisClass::OnPerceptionUpdated);
+	PerceptionComp->ConfigureSense(*SightConfig);
+	PerceptionComp->ConfigureSense(*HearConfig);
+	PerceptionComp->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
 }
 
-void AEnemyAIController::OnPerceptionUpdated(const FActorPerceptionUpdateInfo& UpdateInfo)
+void AEnemyAIController::BeginPlay()
+{
+	UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent());
+	if (CrowdComp)
+	{
+		CrowdComp->SetCrowdSimulationState(ECrowdSimulationState::Enabled);
+		switch (AvoidanceQuality)
+		{
+		case 1: 	CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Low);    break;
+		case 2:	    CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Medium); break;
+		case 3:	    CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::Good);   break;
+		case 4: 	CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::High);   break;
+		default: 	CrowdComp->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::High);   break;
+		}
+		CrowdComp->SetAvoidanceGroup(1);
+		CrowdComp->SetGroupsToAvoid(1);
+		CrowdComp->SetCrowdCollisionQueryRange(CollisionQueryRange);
+	}
+}
+
+void AEnemyAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
 
 }
+
+
+
--- Source/ZombieShooter/Public/Controller/EnemyAIController.h
+++ Source/ZombieShooter/Public/Controller/EnemyAIController.h
@@ -7,13 +7,17 @@
 #include "EnemyAIController.generated.h"
 
 class UAIPerceptionComponent;
+class UAISense;
+class UAISenseConfig_Sight;
+class UAISenseConfig_Hearing;
+class UAISenseConfig_Damage;
 
 UCLASS()
 class ZOMBIESHOOTER_API AEnemyAIController : public AAIController
 {
 	GENERATED_BODY()
 public:
-	AEnemyAIController();
+	AEnemyAIController(const FObjectInitializer& ObjectInitializer);
 	
 #pragma region Components
 
@@ -22,9 +26,35 @@ public:
 
 #pragma endregion
 
-#pragma region Functions
-
-	void OnPerceptionUpdated(const FActorPerceptionUpdateInfo& UpdateInfo);
+#pragma region Configs
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI|Config")
+	UAISenseConfig_Sight* SightConfig;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI|Config")
+	UAISenseConfig_Hearing* HearConfig;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI|Config")
+	UAISenseConfig_Damage* DamageConfig;
 
 #pragma endregion
+
+#pragma region Overrides
+
+	virtual void BeginPlay() override;
+
+#pragma endregion
+
+#pragma region Variables
+
+	UPROPERTY(EditDefaultsOnly, meta = (UIMin = "1", UIMax = "4"), Category = "AI|Crowd")
+	int32 AvoidanceQuality = 4;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI|Crowd")
+	float CollisionQueryRange = 500.f;
+	
+#pragma endregion
+
+#pragma region Functions
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+#pragma endregion
+
 };
